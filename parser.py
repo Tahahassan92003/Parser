efe5d70e856a6353a32cb95b50fcd76ab5051bb2
Taha hassan <133689839+Tahahassan92003@users.noreply.github.com>
2025-02-05 04:41:07 +0500

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,76 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S ->  SS | SS Conj SS | SS P SS
+SS -> NP | VP | NP VP
+NP -> NN | Det NN
+NN -> N | AS N 
+AS -> Adj | Adj AS
+VP -> VV | VV NP | VV NP PP | VV PP 
+VV -> V | Adv V | V Adv
+PP -> P NP | P NP Adv
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    return [w for w in nltk.word_tokenize(sentence.lower())
+        if any(c.isalpha() for c in w)]
+
+
+def np_chunk(tree):
+    return [subtree for subtree in tree.subtrees()
+            if subtree.label() == 'NP']
+
+
+if __name__ == "__main__":
+    main()
